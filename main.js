1d024085772bbe30b3063adcc25eb7502adf9ac2
Manesh Sharma <maneshsharma2710@gmail.com>
2026-04-05 15:42:31 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+require('electron-reload')(__dirname);
+const { app, BrowserWindow } = require('electron');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1000,
+    height: 700,
+  });
+
+  win.loadFile('index.html');
+}
+
+app.whenReady().then(createWindow);
